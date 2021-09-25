b1b8e69512544517a9ef03fd002a5668fc229bb7
MariaSarbaj <marysarbaj@gmail.com>
2021-09-25 20:41:17 +0300

    Add Templates

--- src/express/index.js
+++ src/express/index.js
@@ -1,12 +1,22 @@
 'use strict';
 
 const express = require(`express`);
+const path = require(`path`);
 const router = require(`./routes`);
 
 const DEFAULT_PORT = 8080;
 
+const Dirname = {
+  PUBLIC_DIR: `public`,
+  TEMPLATES: `templates`
+};
+
 const app = express();
 
+app.use(express.static(path.resolve(__dirname, Dirname.PUBLIC_DIR)));
 app.use(router);
+
+app.set(`views`, path.resolve(__dirname, Dirname.TEMPLATES));
+app.set(`view engine`, `pug`);
 
 app.listen(DEFAULT_PORT);
